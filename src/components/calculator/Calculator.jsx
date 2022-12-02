12623d85a3a35607e7f362190899db5a8345379f
z4cope <63551733+z4cope@users.noreply.github.com>
2022-12-02 16:42:23 +0200

    Refactor code to function component

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -1,67 +1,57 @@
-import { PureComponent } from "react";
+import { useState } from "react";
 import "./Calculator.css";
 import calculate from "../../logic/calculate";
-class Calculator extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [calc, setCalc] = useState({ total: 0, next: null, operation: null });
+  const doCalculate = (e) => {
+    let calculations = calculate(calc, e.target.innerText);
+    setCalc({
+      total: calculations.total,
+      next: calculations.next,
+      operation: calculations.operation,
+    });
+  };
 
-  render() {
-    const doCalculate = (e) => {
-      let calculations = calculate(this.state, e.target.innerText);
-      this.setState({
-        total: calculations.total,
-        next: calculations.next,
-        operation: calculations.operation,
-      });
-    };
-
-    return (
-      <div className="calc-wrapper">
-        <p className="calculations">
-          {this.state.total} {this.state.operation} {this.state.next}
-        </p>
-        <div className="keypad">
-          <ul className="keygrid">
-            <li onClick={doCalculate}>AC</li>
-            <li onClick={doCalculate}>+/-</li>
-            <li onClick={doCalculate}>%</li>
-            <li onClick={doCalculate}>÷</li>
-          </ul>
-          <ul className="keygrid">
-            <li onClick={doCalculate}>7</li>
-            <li onClick={doCalculate}>8</li>
-            <li onClick={doCalculate}>9</li>
-            <li onClick={doCalculate}>x</li>
-          </ul>
-          <ul className="keygrid">
-            <li onClick={doCalculate}>4</li>
-            <li onClick={doCalculate}>5</li>
-            <li onClick={doCalculate}>6</li>
-            <li onClick={doCalculate}>-</li>
-          </ul>
-          <ul className="keygrid">
-            <li onClick={doCalculate}>1</li>
-            <li onClick={doCalculate}>2</li>
-            <li onClick={doCalculate}>3</li>
-            <li onClick={doCalculate}>+</li>
-          </ul>
-          <ul className="keygrid last-keygrid">
-            <li className="zero" onClick={doCalculate}>
-              0
-            </li>
-            <li onClick={doCalculate}>.</li>
-            <li onClick={doCalculate}>=</li>
-          </ul>
-        </div>
+  return (
+    <div className="calc-wrapper">
+      <p className="calculations">
+        {calc.total} {calc.operation} {calc.next}
+      </p>
+      <div className="keypad">
+        <ul className="keygrid">
+          <li onClick={doCalculate}>AC</li>
+          <li onClick={doCalculate}>+/-</li>
+          <li onClick={doCalculate}>%</li>
+          <li onClick={doCalculate}>÷</li>
+        </ul>
+        <ul className="keygrid">
+          <li onClick={doCalculate}>7</li>
+          <li onClick={doCalculate}>8</li>
+          <li onClick={doCalculate}>9</li>
+          <li onClick={doCalculate}>x</li>
+        </ul>
+        <ul className="keygrid">
+          <li onClick={doCalculate}>4</li>
+          <li onClick={doCalculate}>5</li>
+          <li onClick={doCalculate}>6</li>
+          <li onClick={doCalculate}>-</li>
+        </ul>
+        <ul className="keygrid">
+          <li onClick={doCalculate}>1</li>
+          <li onClick={doCalculate}>2</li>
+          <li onClick={doCalculate}>3</li>
+          <li onClick={doCalculate}>+</li>
+        </ul>
+        <ul className="keygrid last-keygrid">
+          <li className="zero" onClick={doCalculate}>
+            0
+          </li>
+          <li onClick={doCalculate}>.</li>
+          <li onClick={doCalculate}>=</li>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
