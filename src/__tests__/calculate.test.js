e4aea997b8e29e9ae0a1ef6e45f62dd0b4df9917
z4cope <63551733+z4cope@users.noreply.github.com>
2022-12-08 14:41:43 +0200

    Add linters configs

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,6 +1,6 @@
-import { render } from "@testing-library/react";
-import Calculator from "../components/calculator/Calculator";
-import calculate from "../logic/calculate";
+import { render } from '@testing-library/react';
+import Calculator from '../components/calculator/Calculator';
+import calculate from '../logic/calculate';
 
 /*
 The first test is all about testing the returned object from the calculation function
@@ -9,37 +9,37 @@ The first test is all about testing the returned object from the calculation fun
 - Next
  */
 
-describe("Testing calculations methods", () => {
-  const testingTotal = calculate({ total: 5, next: 5, operation: "+" }, "=");
-  it("Addition method", () => {
-    expect(testingTotal.total).toBe("10");
+describe('Testing calculations methods', () => {
+  const testingTotal = calculate({ total: 5, next: 5, operation: '+' }, '=');
+  it('Addition method', () => {
+    expect(testingTotal.total).toBe('10');
   });
 
   const testingOperation = calculate(
-    { total: 5, next: 5, operation: "+" },
-    "="
+    { total: 5, next: 5, operation: '+' },
+    '=',
   );
-  it("Testing operation", () => {
+  it('Testing operation', () => {
     expect(testingOperation.operation).toBe(null);
   });
 
   const testingNextValue = calculate(
-    { total: 5, next: 5, operation: "+" },
-    "="
+    { total: 5, next: 5, operation: '+' },
+    '=',
   );
-  it("Testing next value", () => {
+  it('Testing next value', () => {
     expect(testingNextValue.next).toBe(null);
   });
 
-  const output = calculate({ total: 20, next: 5, operation: "-" }, "=");
-  test("subtraction", () => {
-    expect(output.total).toBe("15");
+  const output = calculate({ total: 20, next: 5, operation: '-' }, '=');
+  test('subtraction', () => {
+    expect(output.total).toBe('15');
   });
 });
 
-//Component test
-describe("Testing component", () => {
-  it("Renders correctly", () => {
+// Component test
+describe('Testing component', () => {
+  it('Renders correctly', () => {
     const tree = render(<Calculator />);
     expect(tree).toMatchSnapshot();
   });
