de341e8fe8ed11f9a32919397f06c7db6fe51429
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-12-08 13:31:24 +0100

    run test for operate.js

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -1,3 +1,5 @@
+import { render } from "@testing-library/react";
+import Calculator from "../components/calculator/Calculator";
 import calculate from "../logic/calculate";
 
 //Operation
@@ -7,5 +9,18 @@ describe("Testing calculations methods", () => {
   const testMethod = calculate({ total: 5, next: 5, operation: "+" }, "=");
   test("Addition method", () => {
     expect(testMethod.total).toBe("10");
+  });
+
+  const output = calculate({ total: 20, next: 5, operation: "-" }, "=");
+  test("subtraction", () => {
+    expect(output.total).toBe("15");
+  })
+
+});
+//Component test
+describe("Testing component", () => {
+  it("Renders correctly", () => {
+    const tree = render(<Calculator />);
+    expect(tree).toMatchSnapshot();
   });
 });
