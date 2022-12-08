0d8abf1d95817b48f78384e74917ab8b1e82f297
z4cope <63551733+z4cope@users.noreply.github.com>
2022-12-08 14:34:49 +0200

    Add testing for addition method & operation & next value

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -2,13 +2,33 @@ import { render } from "@testing-library/react";
 import Calculator from "../components/calculator/Calculator";
 import calculate from "../logic/calculate";
 
-//Operation
-//Next
+/*
+The first test is all about testing the returned object from the calculation function
+- Total
+- Operation
+- Next
+ */
 
 describe("Testing calculations methods", () => {
-  const testMethod = calculate({ total: 5, next: 5, operation: "+" }, "=");
-  test("Addition method", () => {
-    expect(testMethod.total).toBe("10");
+  const testingTotal = calculate({ total: 5, next: 5, operation: "+" }, "=");
+  it("Addition method", () => {
+    expect(testingTotal.total).toBe("10");
+  });
+
+  const testingOperation = calculate(
+    { total: 5, next: 5, operation: "+" },
+    "="
+  );
+  it("Testing operation", () => {
+    expect(testingOperation.operation).toBe(null);
+  });
+
+  const testingNextValue = calculate(
+    { total: 5, next: 5, operation: "+" },
+    "="
+  );
+  it("Testing next value", () => {
+    expect(testingNextValue.next).toBe(null);
   });
 
   const output = calculate({ total: 20, next: 5, operation: "-" }, "=");
