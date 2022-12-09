bb4737a0881ae9e269f60e629872eca077ae5f4d
z4cope <63551733+z4cope@users.noreply.github.com>
2022-12-09 21:05:45 +0200

    Add navbar in a dynamic way

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './routes/home/Home';
-import CalculatorPage from './routes/calculator/CalculatorPage';
-import Quote from './routes/quote/Quote';
-import Navbar from './components/navbar/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/Home";
+import CalculatorPage from "./routes/calculator/CalculatorPage";
+import Quote from "./routes/quote/Quote";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => (
   <>
-    <Navbar />
     <main>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/quote" element={<Quote />} />
+        <Route element={<Navbar />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<CalculatorPage />} />
+          <Route path="/quote" element={<Quote />} />
+        </Route>
       </Routes>
     </main>
   </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./Navbar.css";
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Math Magicians</h1>
-      <ul className="nav-list">
-        <li>
-          {" "}
-          <Link to="/">Home</Link>{" "}
-        </li>
-        <li>
-          {" "}
-          <Link to="/calculator">Calculator</Link>{" "}
-        </li>
-        <li>
-          {" "}
-          <Link to="/quote">Quote</Link>{" "}
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <h1>Math Magicians</h1>
+        <ul className="nav-list">
+          <li>
+            {" "}
+            <Link to="/">Home</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/calculator">Calculator</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/quote">Quote</Link>{" "}
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
   );
 };
 
